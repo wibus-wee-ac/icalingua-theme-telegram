5674badc6174aea0aa263a20dd41230f7783ce89
wibus-wee <1596355173@qq.com>
2023-07-02 17:49:14 +0800

    fix: for js autocomplete

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 # Check if the system is macOS
 if [ "$(uname)" == "Darwin" ]; then
-   cp -r ./src/* ~/Library/Application\ Support/icalingua
-   mv ~/Library/Application\ Support/icalingua/src ~/Library/Application\ Support/icalingua/telegram-theme
+   cp -r ./telegram-theme/* ~/Library/Application\ Support/icalingua
    cp style.css ~/Library/Application\ Support/icalingua
    cp addon.js ~/Library/Application\ Support/icalingua
    cp main.js ~/Library/Application\ Support/icalingua
    echo "Done."
 # Check if the system is Linux
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-   cp -r ./src ~/.config/icalingua
-   mv ~/.config/icalingua/src ~/.config/icalingua/telegram-theme
+   cp -r ./telegram-theme ~/.config/icalingua
    cp style.css ~/.config/icalingua
    cp addon.js ~/.config/icalingua
    cp main.js ~/.config/icalingua
