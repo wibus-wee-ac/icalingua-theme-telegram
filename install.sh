f1197246561c6a5f9fcef4877c3c7946c02f3b8c
timochan <i@timochan.cn>
2023-07-02 17:39:06 +0800

    add: macOS cp

--- install.sh
+++ install.sh
@@ -3,13 +3,15 @@
 if [ "$(uname)" == "Darwin" ]; then
    cp -r ./src/* ~/Library/Application\ Support/icalingua
    cp style.css ~/Library/Application\ Support/icalingua
+   cp addon.js ~/Library/Application\ Support/icalingua
+   cp main.js ~/Library/Application\ Support/icalingua
    echo "Done."
 # Check if the system is Linux
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
    cp -r ./src ~/.config/icalingua
    cp style.css ~/.config/icalingua
-   mv addon.js ~/.config/icalingua
-   mv main.js  ~/.config/icalingua
+   cp addon.js ~/.config/icalingua
+   cp main.js ~/.config/icalingua
    echo "Done."
 
 else
