0e6ffe12e7ac2a2ba6b31c948bd4389d8908d5ae
wibus-wee <1596355173@qq.com>
2023-07-05 16:46:12 +0800

    perf: beauty dev server

--- dev-server.js
+++ dev-server.js
@@ -1,16 +1,44 @@
 import { execSync } from "child_process";
-import { watch } from "fs";
-
+import { readFileSync, readdirSync, watch } from "fs";
+import chalk from "chalk";
+import path from "path";
+import consola from "consola";
+console.clear()
 const watcher = ["addon.js", "style.scss"]
+const cache = {};
+const pwd = execSync("pwd", { encoding: "utf-8" }).trim()
+function cacheFiles(dir) {
+  readdirSync(dir, { withFileTypes: true }).forEach((file) => {
+    if (file.isDirectory()) {
+      cacheFiles(`${dir}/${file.name}`)
+    } else {
+      // consola.info(`${chalk.cyan("[Cache]")} Caching ${chalk.green(file.name)}...`)
+      cache[`${dir}/${file.name}`] = readFileSync(`${dir}/${file.name}`, { encoding: "utf-8" })
+    }
+  })
+}
+cacheFiles(path.resolve(pwd, "./src"))
+watcher.forEach((file) => {
+  // consola.info(`${chalk.cyan("[Cache]")} Caching ${chalk.green(file)}...`)
+  cache[`${path.resolve(pwd)}/${file}`] = readFileSync(`${file}`, { encoding: "utf-8" })
+})
+
+consola.info(`${chalk.cyan("[Cache]")} Caching complete\n`);
 
 watch("./", { recursive: true }, (eventType, filename) => {
   if (watcher.includes(filename) || filename.includes("src")) {
-    console.log(`${filename} changed, recompiling...`)
+    // 比对文件内容是否发生变化
+    if (cache[`${path.resolve(pwd)}/${filename}`] === readFileSync(`${path.resolve(pwd)}/${filename}`, { encoding: "utf-8" })) {
+      return
+    }
+    // 更新缓存
+    cache[filename] = readFileSync(filename)
+    consola.info(`${chalk.cyan("[Cache]")} ${chalk.green(filename)} updated`)
+    // consola.info(`${filename} changed, recompiling...`)
+    consola.info(`${chalk.bold(chalk.blue("[Dev]"))} Recompiling...`)
     execSync("sh replace.sh");
-    console.log("Recompilation complete")
+    // consola.info("Recompilation complete")
   }
 })
-console.clear()
-console.log("=== Icalingua Theme Dev Server === \n")
-console.log("Watching for file changes...")
-console.log("Press Ctrl+C to exit")
+consola.info(`${chalk.bold(chalk.blue("[Dev]"))} Watching for file changes...`)
+consola.info(`${chalk.bold(chalk.blue("[Dev]"))} Press ${chalk.green("Ctrl + C")} to exit`)
