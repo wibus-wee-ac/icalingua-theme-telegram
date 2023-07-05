31e68995cc8330664d91498749c2e05eac5af436
wibus-wee <1596355173@qq.com>
2023-07-05 14:52:54 +0800

    renam

--- src/core/theme-listener.ts
+++ src/core/theme-listener.ts
@@ -5,7 +5,7 @@ const fs = require('fs');
 export function themeListener() {
   // 此处只能选择忽略自定义 config.yaml 路径的用户，因为暂时没有办法获取到用户的 config.yaml 路径（似乎）
   if (!window.theme.location) { return; }
-  fs.watch(path.resolve(window.theme.location, 'config.yaml'), (eventType, filename) => {
+  fs.watch(path.resolve(window.theme.location, 'config.yaml'), (eventType: string, filename: any) => {
     if (eventType === 'change') {
       
     }
