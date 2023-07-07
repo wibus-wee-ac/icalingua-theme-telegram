e1c28b2adb2315d7c9382f4395909949c5ae512f
wibus-wee <1596355173@qq.com>
2023-07-07 18:35:06 +0800

    chore: comment sections of unnecessary code

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ window.theme = {
     "better-image-display",
     "fix-message-content-width",
   ],
-  dev: true,
+  dev: false,
   core: [
     "inject-custom-root-style",
     "listen-rooms-panel-drag-event",
     "modify-chat-box-interval",
     "refined-image-gallery",
-    "file-changes-listener",
+    // "file-changes-listener", // 建议只在开发时开启
     "theme-listener",
   ],
   manual: true,
